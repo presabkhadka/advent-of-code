3d4feeca28036afbd30a0c0ffc8d3df612ec087b
Presab <presabkhadka30@gmail.com>
2025-08-08 08:54:50 +0545

    day1: completed

---
+++ day1.js
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import { argv0 } from 'process'
+
+const arrOne = []
+const arrTwo = []
+let finalAnswer = 0
+
+fs.readFile('ques.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.log('error while reading the file')
+    return
+  }
+
+  function compareNumbers(a, b) {
+    return a - b
+  }
+
+  const lines = data.trim().split("\n")
+  lines.forEach(line => {
+    const [col1, col2] = line.trim().split(/\s+/)
+    arrOne.push(Number(col1))
+    arrTwo.push(Number(col2))
+  })
+
+  arrOne.sort(compareNumbers)
+  arrTwo.sort(compareNumbers)
+
+  for (let i = 0; i < arrOne.length; i++) {
+    let difference = Math.abs(arrOne[i] - arrTwo[i])
+    console.log('difference', difference)
+    finalAnswer = finalAnswer + difference
+  }
+  console.log('Ans -> ', finalAnswer)
+})
+
